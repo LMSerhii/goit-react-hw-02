9b40361c9c1047d1cf94c8ff04b3510e4890abae
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-01-16 15:48:07 +0200

    local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { Description } from './Description/Description';
 import { Options } from './Options/Options';
 import { Feedback } from './Feedback/Feedback';
+import { load, save } from '../js/storage';
 
 function App() {
-  const [assessments, setAssessments] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [assessments, setAssessments] = useState(() => {
+    const savedAssessments = load('saved-assessments');
+
+    if (savedAssessments) {
+      return savedAssessments;
+    }
+
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
   });
+
+  useEffect(() => {
+    save('saved-assessments', assessments);
+  }, [assessments]);
+
   return (
     <div className="container appWrapper">
       <Description
